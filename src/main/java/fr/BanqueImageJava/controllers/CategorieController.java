00dd43931146fafd84011a7ae49bb70da3acc432
Anthony <a.leclercq55@live.fr>
2021-03-01 22:35:14 +0100

    Création des tests du ServiceImage

---
+++ src/main/java/fr/BanqueImageJava/controllers/CategorieController.java
@@ -0,0 +1,2 @@
+package fr.BanqueImageJava.controllers;public class CategorieController {
+}
---
+++ src/test/java/fr/BanqueImageJava/services/ImageServiceImplTest.java
@@ -0,0 +1,34 @@
+package fr.BanqueImageJava.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageServiceTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
